62bb207439bbdcb0274191269e4fab215af2ba7a
iteamurr <iteamurr@gmail.com>
2021-11-04 00:47:30 +0300

    fix: deploy errors

--- src/api/tools.py
+++ src/api/tools.py
@@ -9,7 +9,7 @@ def create_chat_message(msg_template):
 
   for item in message_items:
     if item in emotes:
-      with open('emotes.json', 'r') as emotes_json:
+      with open('./emotes.json', 'r') as emotes_json:
         emote_src = json.load(emotes_json)[item[1:-1]]
       message += f'<img src="{emote_src}" class="user-message-emote">'
     else:
