2a6da51408d9efee25e9e945819e2be991161cb5
iteamurr <iteamurr@gmail.com>
2021-11-03 23:41:23 +0300

    feat: create chat api

---
+++ src/api/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(40), unique=True, nullable=False)
+  password = db.Column(db.String, nullable=False)
+
+class Room(db.Model):
+  __tablename__ = 'rooms'
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, nullable=False)
+  chat_link = db.Column(db.String(50), nullable=False)
+
+class Chat(db.Model):
+  __tablename__ = 'chats'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(70), nullable=False)
+  link = db.Column(db.String(50), unique=True, nullable=False)
---
+++ src/api/tools.py
@@ -0,0 +1,18 @@
+import re
+import json
+
+
+def create_chat_message(msg_template):
+  message = ''
+  message_items = re.split(r'(:\w+:)', msg_template)
+  emotes = re.findall(r'(:\w+:)', msg_template)
+
+  for item in message_items:
+    if item in emotes:
+      with open('api/emotes.json', 'r') as emotes_json:
+        emote_src = json.load(emotes_json)[item[1:-1]]
+      message += f'<img src="/static/emotes/{emote_src}" class="user-message-emote">'
+    else:
+      message += f'<span class="user-message-item">{item}</span>'
+
+  return message
