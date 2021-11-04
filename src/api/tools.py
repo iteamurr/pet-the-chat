99915c2cf7f8600cd92882ef33a81d066ad3b682
iteamurr <iteamurr@gmail.com>
2021-11-05 01:33:55 +0300

    fix: non-existent emote

--- src/api/tools.py
+++ src/api/tools.py
@@ -1,5 +1,4 @@
 import re
-import os
 import json
 
 
@@ -10,9 +9,13 @@ def create_chat_message(msg_template):
 
   for item in message_items:
     if item in emotes:
-      with open('/app/src/api/emotes.json', 'r') as emotes_json:
-        emote_src = json.load(emotes_json)[item[1:-1]]
-      message += f'<img src="{emote_src}" class="user-message-emote">'
+      with open('/app/src/api/emotes.json', 'r') as emotes_json_file:
+        emotes_json, emote = json.load(emotes_json_file), item[1:-1]
+        if emote in emotes_json:
+          emote_src = emotes_json[emote]
+          message += f'<img src="{emote_src}" class="user-message-emote">'
+        else:
+          message += f'<span class="user-message-item">{item}</span>'
     else:
       message += f'<span class="user-message-item">{item}</span>'
 
