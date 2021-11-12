c82624c02073b0a4b586d03e41c20ac262708044
iteamurr <iteamurr@gmail.com>
2021-11-12 23:11:21 +0300

    feat: add a new chat creation form

--- src/static/scripts/chat.js
+++ src/static/scripts/chat.js
@@ -6,61 +6,89 @@ $(document).ready(function () {
   var username_color = "#9ACD32";
   joinChat(chat_link);
 
-  socket.on('create_message', function (data) {
-    var username_div = `<div class="username" style="color: ${data['username_color']};">${data['username']}:</div>`;
-    var message_div = `<div class="message-items">${data['data']}</div>`;
-    var user_message = `<div class="shadow" id="user-message">${username_div}${message_div}</div>`;
-    $('#chat').append(user_message);
+  socket.on("create_message", function (data) {
+    var username_div = `<div class="username" style="color: ${data["username_color"]};">${data["username"]}:</div>`;
+    var message_div = `<div class="message-items">${data["data"]}</div>`;
+    var user_message = `<div class="user-message">${username_div}${message_div}</div>`;
+    $(".chat-messages").append(user_message);
     scrollChat();
   });
 
-  socket.on('new_chat', function (data) {
-    var chat_name_p = `<p class="chat-name" id="${data['chat_link']}">${data['chat_name']}</p>`;
+  socket.on("system_message", function (data) {
+    var message_div = `<div class="system-items">${data["data"]}</div>`;
+    var system_message = `<div class="system-message">${message_div}</div>`;
+    $(".chat-messages").append(system_message);
+    scrollChat();
+    $(".chat-header-name").children("span").eq(0).text(data["chat_name"]);
+  });
+
+  socket.on("new_chat", function (data) {
+    var chat_name_p = `<p class="chat-name" id="${data["chat_link"]}">${data["chat_name"]}</p>`;
     var chat_div = `<div class="select-chat">${chat_name_p}</div>`;
-    $('#chats').append(chat_div);
+    $(".chats-content").append(chat_div);
+    joinChat(data["chat_link"]);
   });
 
-  $('#send_message').on('click', function () {
-    if ($.trim($('#textarea').val()).length > 0) {
-      socket.emit('send_message', { msg: $('#textarea').val(), username: username, chat_link: chat_link, username_color: username_color });
-      $('#textarea').val('');
+  $("#send-message").on("click", function () {
+    if ($.trim($("#textarea").val()).length > 0) {
+      socket.emit("send_message", {
+        msg: $("#textarea").val(),
+        username: username,
+        chat_link: chat_link,
+        username_color: username_color,
+      });
+      $("#textarea").val("");
     }
     return false;
   });
 
-  $('#submit-create-chat').on('click', function () {
-    if ($('#new-chat-name').val().length > 0) {
-      socket.emit('create_chat', { chat_name: $('#new-chat-name').val(), admin: username, current_user: socket.id });
-    }
+  $("#submit-chat-creation").on("click", function () {
+    if ($("#enter-chat-name").val().length > 0) {
+      socket.emit("create_chat", {
+        chat_name: $("#enter-chat-name").val(),
+        admin: username,
+        current_user: socket.id,
+      });
+      $("#enter-chat-name").val("");
+      $("#cancel-chat-creation").click();
+    } else $(".create-chat-form-content").children("small").eq(0).attr("style", "visibility: visible");
     return false;
   });
 
-  $('#chats').on('click', '.select-chat', function () {
-    selected_chat = $(this).children('.chat-name').attr('id');
+  $(".chats").on("click", ".select-chat", function () {
+    selected_chat = $(this).children(".chat-name").attr("id");
     if (selected_chat !== chat_link) {
       leaveChat(chat_link);
       joinChat(selected_chat);
-      chat_link = selected_chat;
     }
     return false;
   });
 
   function joinChat(chat) {
-    $('#chat').empty();
+    $(".chat-messages").empty();
     username_color = getNewUsernameColor();
-    socket.emit('join', { username: username, chat_link: chat, username_color: username_color });
+    socket.emit("join", {
+      username: username,
+      chat_link: chat,
+      username_color: username_color,
+    });
+    chat_link = chat;
   }
 
   function leaveChat(chat) {
-    socket.emit('leave', { username: username, chat_link: chat, username_color: username_color });
+    socket.emit("leave", {
+      username: username,
+      chat_link: chat,
+      username_color: username_color,
+    });
   }
 
   function scrollChat() {
-    var new_scroll = $('#chat').prop('scrollTop');
+    var new_scroll = $(".chat-messages").prop("scrollTop");
     if (new_scroll >= scroll) {
-      $('#chat').children().last()[0].scrollIntoView({
+      $(".chat-messages").children().last()[0].scrollIntoView({
         behavior: "smooth",
-        block: "end"
+        block: "end",
       });
       scroll = new_scroll;
     }
@@ -68,11 +96,20 @@ $(document).ready(function () {
 
   function getNewUsernameColor() {
     const colors = [
-      "#FF0000", "#008000", "#B22222",
-      "#FF7F50", "#9ACD32", "#FF4500",
-      "#2E8B57", "#DAA520", "#D2691E",
-      "#5F9EA0", "#1E90FF", "#FF69B4",
-      "#8A2BE2", "#00FF7F"
+      "#FF0000",
+      "#008000",
+      "#B22222",
+      "#FF7F50",
+      "#9ACD32",
+      "#FF4500",
+      "#2E8B57",
+      "#DAA520",
+      "#D2691E",
+      "#5F9EA0",
+      "#1E90FF",
+      "#FF69B4",
+      "#8A2BE2",
+      "#00FF7F",
     ];
     const random = Math.floor(Math.random() * colors.length);
     return colors[random];
--- src/static/scripts/tools.js
+++ src/static/scripts/tools.js
@@ -2,54 +2,59 @@ $(document).ready(function () {
   $("#textarea").keypress(function (e) {
     if (e.which === 13 && !e.shiftKey) {
       e.preventDefault();
-      $('#send_message').click();
-      $('#textarea').attr('rows', 1);
+      $("#send-message").click();
+      $("#textarea").css("height", "50px");
     }
   });
 
-  $("#textarea").keypress(function (e) {
-    if (e.which === 13 && e.shiftKey) {
-      if ($('#textarea').attr('rows') < 5)
-        $('#textarea').attr('rows', parseInt($('#textarea').attr('rows')) + 1);
-    }
+  $("#textarea").on("input", function () {
+    $("#textarea").css("height", "50px");
+    $("#textarea").css("height", $("#textarea").prop("scrollHeight") + "px");
   });
 
-  $("#textarea").on('input', function () {
-    if ($('#textarea').val().length === 0)
-      $('#textarea').attr('rows', 1);
+  $(".chat-messages-field").on("scroll", function () {
+    if (
+      $(".chat-messages-field").prop("scrollHeight") -
+        $(".chat-messages-field").prop("scrollTop") >
+      1200
+    )
+      $(".scroll-down").addClass("active");
+    else $(".scroll-down").removeClass("active");
   });
 
-  $("#chat").on('scroll', function () {
-    if ($('#chat').prop('scrollHeight') - $('#chat').prop('scrollTop') > 1200)
-      $('.scroll-down').toggleClass('scroll-down scroll-down-flex');
-    else
-      $('.scroll-down-flex').toggleClass('scroll-down-flex scroll-down');
+  $(".scroll-down").on("click", function () {
+    $(".chat-messages-field")
+      .children(".chat-messages")
+      .children()
+      .last()[0]
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
   });
 
-  $(".scroll-down").on('click', function () {
-    $('#chat').children().last()[0].scrollIntoView({
-      behavior: "smooth",
-      block: "end"
-    });
+  $(".chats-header-icon").click(function (event) {
+    $(".select-chat").toggleClass("active");
+    $(".chats").toggleClass("active");
+    $(".content-back").toggleClass("active");
+    $(".chats-header").toggleClass("active");
   });
 
-  $('.chats-header-icon').click(function (event) {
-    $('.select-chat').toggleClass('active');
-    $('.chats-menu').toggleClass('active');
-    $('.content-back').toggleClass('active');
-    $('.chats-header-menu').toggleClass('active');
+  $(".emote-btn").on("click", function (event) {
+    $(".emotes-menu").toggleClass("active");
   });
 
-  $('#emote-btn').click(function (event) {
-    $('.all-emotes').toggleClass('active');
+  $(".emotes-menu").on("click", ".get-emote", function () {
+    text = $(this).children("span").text();
+    if ($("#textarea").val().length > 0)
+      $("#textarea")
+        .val($("#textarea").val() + ` ${text} `)
+        .trigger("input");
+    else $("#textarea").val(`${text} `).trigger("input");
   });
 
-  $('.all-emotes').on('click', '.get-emote', function () {
-    text = $(this).children('span').text();
-    if ($('#textarea').val().length > 0)
-      $('#textarea').val($('#textarea').val() + ` ${text} `);
-    else
-      $('#textarea').val(`${text} `)
+  $(".create-chat").on("click", function (e) {
+    $(".create-chat-form").toggleClass("active");
+    $(".create-chat-form-content-back").toggleClass("active");
   });
-  return false;
 });
